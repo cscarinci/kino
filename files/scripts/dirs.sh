8092dbb62e65e97daaf7fbea1388e17f4ce2f32f
Carlos Scarinci <cscarinci@dgist.ac.kr>
2026-05-22 20:59:50 +0900

    initial kinoite image

---
+++ files/scripts/dirs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+mkdir -p /etc/skel/0x_Inbox
+mkdir -p /etc/skel/1x_System
+mkdir -p /etc/skel/2x_Work
+mkdir -p /etc/skel/3x_Personal
+mkdir -p /etc/skel/4x_References
+mkdir -p /etc/skel/5x_Media
+mkdir -p /etc/skel/9x_Archive
